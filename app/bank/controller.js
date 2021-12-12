50f1da0cfe4e68644b478db531852b2875878f4f
fathurzoy <fathurrahmanf123@gmail.com>
2021-12-12 17:06:55 +0700

    Mengelola halaman bank (CRUD)→ Create

---
+++ app/bank/controller.js
@@ -0,0 +1,116 @@
+const Bank = require("./model");
+
+module.exports = {
+  index: async (req, res) => {
+    try {
+      const alertMessage = req.flash("alertMessage");
+      const alertStatus = req.flash("alertStatus");
+
+      const alert = { message: alertMessage, status: alertStatus };
+      const bank = await Bank.find();
+
+      console.log("alert >>");
+      console.log(alert);
+
+      res.render("admin/bank/view_bank", {
+        bank,
+        alert,
+      });
+    } catch (err) {
+      req.flash("alertMessage", `${err.message}`);
+      req.flash("alertStatus", "danger");
+      res.redirect("/bank");
+      // console.log(err);
+    }
+  },
+  viewCreate: async (req, res) => {
+    try {
+      res.render("admin/bank/create");
+    } catch (err) {
+      req.flash("alertMessage", `${err.message}`);
+      req.flash("alertStatus", "danger");
+      res.redirect("/bank");
+      // console.log(err);
+    }
+  },
+
+  actionCreate: async (req, res) => {
+    try {
+      const { name, nameBank, noRekening } = req.body;
+
+      let bank = await Bank({ name, nameBank, noRekening });
+      await bank.save();
+
+      req.flash("alertMessage", "Berhasil tambah bank");
+      req.flash("alertStatus", "success");
+
+      res.redirect("/bank");
+    } catch (err) {
+      req.flash("alertMessage", `${err.message}`);
+      req.flash("alertStatus", "danger");
+      res.redirect("/bank");
+      // console.log(err);
+    }
+  },
+
+  //   viewEdit: async (req, res) => {
+  //     try {
+  //       const { id } = req.params;
+
+  //       const bank = await Bank.findOne({ _id: id });
+  //       // console.log(bank);
+
+  //       res.render("admin/bank/edit", {
+  //         bank,
+  //       });
+  //     } catch (err) {
+  //       req.flash("alertMessage", `${err.message}`);
+  //       req.flash("alertStatus", "danger");
+  //       res.redirect("/bank");
+  //       // console.log(err);
+  //     }
+  //   },
+
+  //   actionEdit: async (req, res) => {
+  //     try {
+  //       const { id } = req.params;
+  //       const { coinName, coinQuantity, price } = req.body;
+
+  //       const bank = await Bank.findOneAndUpdate(
+  //         {
+  //           _id: id,
+  //         },
+  //         { coinName, coinQuantity, price }
+  //       );
+
+  //       req.flash("alertMessage", "Berhasil ubah bank");
+  //       req.flash("alertStatus", "success");
+
+  //       res.redirect("/bank");
+  //     } catch (err) {
+  //       req.flash("alertMessage", `${err.message}`);
+  //       req.flash("alertStatus", "danger");
+  //       res.redirect("/bank");
+  //       // console.log(err);
+  //     }
+  //   },
+
+  //   actionDelete: async (req, res) => {
+  //     try {
+  //       const { id } = req.params;
+
+  //       const bank = await Bank.findOneAndRemove({
+  //         _id: id,
+  //       });
+  //       req.flash("alertMessage", "Berhasil hapus bank");
+  //       req.flash("alertStatus", "success");
+
+  //       res.redirect("/bank");
+  //     } catch (err) {
+  //       req.flash("alertMessage", `${err.message}`);
+  //       req.flash("alertStatus", "danger");
+  //       res.redirect("/bank");
+  //       // console.log(err);
+  //     }
+  //   },
+};
