bf7c2a46b55db7c8ca749f05920aaae4cdd87f80
fathurzoy <fathurrahmanf123@gmail.com>
2021-12-13 00:31:39 +0700

    Mengelola halaman transaksi

---
+++ app/transaction/router.js
@@ -0,0 +1,26 @@
+var express = require("express");
+var router = express.Router();
+const {
+  index,
+  viewCreate,
+  actionCreate,
+  viewEdit,
+  actionEdit,
+  actionDelete,
+} = require("./controller");
+
+// /* GET home page. */
+// router.get("/", function (req, res, next) {
+//   res.render("index", { title: "Express" });
+// });
+const { isLoginAdmin } = require("../middleware/auth");
+
+router.use(isLoginAdmin);
+router.get("/", index);
+// router.get("/create", viewCreate);
+// router.post("/create", actionCreate);
+// router.get("/edit/:id", viewEdit);
+// router.put("/edit/:id", actionEdit);
+// router.delete("/delete/:id", actionDelete);
+
+module.exports = router;
